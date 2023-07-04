4bad80f2fa48486a3b46165bd68de34c237ceddd
Yaodong Li <mail@liyaodong.com>
2023-07-04 23:31:52 +0800

    Add basic styling for text input

--- src/TextField.tsx
+++ src/TextField.tsx
@@ -1,5 +1,5 @@
-import React, { Ref } from 'react';
-import { HTMLFieldProps, connectField, filterDOMProps } from 'uniforms';
+import React, { Ref } from "react";
+import { HTMLFieldProps, connectField, filterDOMProps } from "uniforms";
 
 export type TextFieldProps = HTMLFieldProps<
   string,
@@ -23,24 +23,32 @@ function Text({
 }: TextFieldProps) {
   return (
     <div {...filterDOMProps(props)}>
-      {label && <label htmlFor={id}>{label}</label>}
+      {label && (
+        <label
+          className="block text-sm font-medium leading-6 text-gray-900"
+          htmlFor={id}
+        >
+          {label}
+        </label>
+      )}
 
       <input
+        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
         autoComplete={autoComplete}
         disabled={disabled}
         id={id}
         name={name}
-        onChange={event => onChange(event.target.value)}
+        onChange={(event) => onChange(event.target.value)}
         placeholder={placeholder}
         readOnly={readOnly}
         ref={inputRef}
         type={type}
-        value={value ?? ''}
+        value={value ?? ""}
       />
     </div>
   );
 }
 
-Text.defaultProps = { type: 'text' };
+Text.defaultProps = { type: "text" };
 
-export default connectField<TextFieldProps>(Text, { kind: 'leaf' });
+export default connectField<TextFieldProps>(Text, { kind: "leaf" });
