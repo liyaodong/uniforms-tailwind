e324b586332294e719a52d8a1c01064ca41b8715
Yaodong Li <mail@liyaodong.com>
2023-07-05 00:06:05 +0800

    Remove spacing between select

--- src/SelectField.tsx
+++ src/SelectField.tsx
@@ -78,7 +78,7 @@ function Select({
         ))
       ) : (
         <select
-          className="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          className="block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
           disabled={disabled}
           id={id}
           multiple={multiple}
